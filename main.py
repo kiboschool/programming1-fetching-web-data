4057d747404c5f1bc7c7b81feecac9c2a2581343
Muawiya Asali <muawiya.asali@gm.com>
2022-11-15 23:36:11 +0200

    modifying solution to use jumia

--- main.py
+++ main.py
@@ -2,42 +2,37 @@ from bs4 import BeautifulSoup
 import requests
 
 # Constant Values
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
-           'Accept-Language': 'en-US, en;q=0.5'
-}
+base_url = 'https://www.jumia.com'
+filter = {
+            'display_size': '15.0--15.6',
+            'operating_system': 'Windows+10',
+            'price': '125000-285000',
+            'hdd_size': '1000',
+            'cpu_speed': '3.00--2.20'
+            }
 
-amzn_webpage_link = 'https://tinyurl.com/2mn2tb4c'
-flipkart_webpage_link = 'https://tinyurl.com/2atd9rr4'
-
-def amzn_product_list(amzn_webpage_link: str):
-    response = requests.get(amzn_webpage_link, headers=headers)
+def fetch_product_data(country_code, filter, product_name):
+    url = f"{base_url}.{country_code}/{product_name}/?{filter}"
+    response = requests.get(url)
 
     if response.status_code == 200 or response.status_code == 201:
         content = response.text
         soup = BeautifulSoup(content, "html.parser")
-        for i, name in enumerate(soup.findAll('span', attrs={'class':'a-size-medium a-color-base a-text-normal'})):
+        for i, name in enumerate(soup.findAll('h3', attrs={'class':'name'})):
+            print(f"[Laptop {i}]: {name.get_text()}\n")
+        for i, name in enumerate(soup.findAll('div', attrs={'class':'prc'})):
             print(f"[Laptop {i}]: {name.get_text()}\n")
     else:
         print(f'Failed to access webpage with error {response.status_code}')
 
 
-def flipkart_product_list(flipkart_webpage_link):
-    response = requests.get(flipkart_webpage_link, headers=headers)
+def make_filter(filter_params):
+    return ("&".join(f'{k}={v}' for k,v in filter.items()))
 
-    if response.status_code == 200 or response.status_code == 201:
-        content = response.text
-        soup = BeautifulSoup(content, "html.parser")
-        for i, name in enumerate(soup.findAll('div', attrs={'class':'_4rR01T'})):
-            print(f"[Laptop {i}]: {name.get_text()}\n")
-    else:
-        print(f'Failed to access webpage with error {response.status_code}')
-
-
-def fetch_products():
-    print('Amazon Laptop Filter:\n')
-    amzn_product_list(amzn_webpage_link=amzn_webpage_link)
-    print('Flipkart Laptop Filter:\n')
-    flipkart_product_list(flipkart_webpage_link=flipkart_webpage_link)
+def save_to_file():
+    pass
 
 if __name__ == '__main__':
-    fetch_products()
+    # fetch_products()
+    
+    fetch_product_data(country_code='ng', filter=make_filter(filter_params=filter), product_name='laptops')
